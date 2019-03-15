7dfefc3c9ec900a2a6d21b5bec4c72b2ca941c2d
Chenbo Yi <yichenbo@outlook.com>
2019-03-15 13:51:46 +0000

    all test passing for fizzbuzz co-author Andrew

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,7 @@
 def fizzbuzz(number)
+  return 'fizzbuzz' if (number % 15).zero?
   return 'buzz' if (number % 5).zero?
   (number % 3).zero? ? 'fizz' : number
 end
+
+(1..100).each{|i| p fizzbuzz(i)}
